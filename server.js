9bbc8252747a0c86805b1a0dad62f8be93d19093
Mik Dadayan <dadayan.mikayel@gmail.com>
2021-03-21 15:53:51 +0400

    Created course model

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-const courses = require("./routes/courses");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+const courses = require("./routes/courses");
 
 const app = express();
 
@@ -17,6 +22,16 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/courses", courses);
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT} ...`);
+
+const server = app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT} ...`.yellow.bold
+  );
+});
+
+// Hande unhandled promise rejection
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
 });

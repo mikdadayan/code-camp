53c0da6d54b1fd31b55f56ea96a0f60122a3af1e
Mik Dadayan <dadayan.mikayel@gmail.com>
2021-04-20 13:35:54 +0400

    Created login endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -17,3 +17,34 @@ exports.register = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({ success: true, token });
 });
+
+// @desc   Login User
+// @route  Post /api/v1/auth/login
+// @access Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  // Check if pasword matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  //Create token
+  const token = user.getSignedJwtToken();
+  console.log(token);
+
+  res.status(200).json({ success: true, token });
+});

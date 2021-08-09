d8fea7b96dae2f3658fa047eb26537abd95aa2be
Mik Dadayan <dadayan.mikayel@gmail.com>
2021-08-09 20:41:03 +0400

    Created log out route, controller

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  logout,
 } = require("../controllers/auth");
 const { protect } = require("../middlewares/auth");
 
@@ -13,6 +14,7 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", protect, logout);
 router.get("/getMe", protect, getMe);
 router.post("/forgotpassword", forgotPassword);
 router.put("/resetpassword/:resettoken", resetPassword);
